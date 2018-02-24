957c867e1f4e1c6f7cabf05ea41d31861bd864b5
Aeris94 <ada.wronowska@gmail.com>
2018-02-24 10:28:54 +0100

    Update LeapYear.cs

--- usingObjects/LeapYear.cs
+++ usingObjects/LeapYear.cs
@@ -1,1 +1,35 @@
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+
+//Write a program, which reads from the console a year and checks if it is a leap year
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj rok: ");
+            int year;
+            while(!int.TryParse(Console.ReadLine(), out year))
+            {
+                Console.WriteLine("Invalid data! Try again: ");
+            }
+
+            if(DateTime.IsLeapYear(year))
+            {
+                Console.WriteLine("It is a leap year.");
+            }
+            else
+            {
+                Console.WriteLine("It is not a leap year.");
+            }
+        }
+    }
+}

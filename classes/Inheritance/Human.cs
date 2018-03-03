b884e73ce6f4fd80f17dc41696a25705bba831c7
Aeris94 <ada.wronowska@gmail.com>
2018-03-03 09:36:27 +0100

    Update Human.cs

--- classes/Inheritance/Human.cs
+++ classes/Inheritance/Human.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Inheritance
 {
-    class Human<T> where T: IComparable<T>
+    abstract class Human<T> where T: IComparable<T>
     {
         private string firstName;
         private string lastName;
